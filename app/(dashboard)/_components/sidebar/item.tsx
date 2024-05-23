835cc605e6ed92b4b681e58f8d8149e72851586b
Jatin Ranpariya <jatinranpariya1510@gmail.com>
2024-05-23 13:40:01 +0530

    Setup Organization List - Clerk + Frontend

---
+++ app/(dashboard)/_components/sidebar/item.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import Image from "next/image";
+
+import { useOrganization, useOrganizationList } from "@clerk/nextjs";
+import { cn } from "@/lib/utils";
+
+interface ItemProps {
+    id: string;
+    name: string;
+    imageUrl: string;
+}
+
+export const Item = ({ id, name, imageUrl }: ItemProps) => {
+    const { organization } = useOrganization();
+    const { setActive } = useOrganizationList();
+    const isActive = organization?.id === id;
+    const onClick = () => {
+        if (!setActive) return;
+        setActive({ organization: id });
+    };
+    return (
+        <div className="aspect-square relative">
+            <Image
+                fill
+                src={imageUrl}
+                onClick={onClick}
+                alt={name}
+                className={cn(
+                    "rounded-md cursor-pointer opacity-75 hover:opacity-100 transition",
+                    isActive && "opacity-100"
+                )}
+            />
+        </div>
+    );
+};
---
+++ app/(dashboard)/_components/sidebar/new-button.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+// import { Hint } from "@/components/hint";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { CreateOrganization } from "@clerk/nextjs";
+import { Plus } from "lucide-react";
+
+export const NewButton = () => {
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                <div className="aspect-square">
+                    {/* <Hint
+                        label="Create organization"
+                        side="right"
+                        align="start"
+                        sideOffset={18}
+                    >
+                        <div className="bg-white/25 h-full w-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition">
+                            <Plus className="text-white" />
+                        </div>
+                    </Hint> */}
+                    <div className="bg-white/25 h-full w-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition">
+                        <Plus className="text-white" />
+                    </div>
+                </div>
+            </DialogTrigger>
+            <DialogContent className="p-0 bg-transparent border-none max-w-[480px]">
+                <CreateOrganization />
+            </DialogContent>
+        </Dialog>
+    );
+};
