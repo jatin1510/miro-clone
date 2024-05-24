5ef192e79616dc2cc07ea50df1f69cbd04f586d3
Jatin Ranpariya <jatinranpariya1510@gmail.com>
2024-05-24 09:37:48 +0530

    Navbar Build | Search + Mobile view: Organization Switcher

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,12 +1,37 @@
 "use client";
 
-import { UserButton } from "@clerk/nextjs";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import { SearchInput } from "./search-input";
 
 export const Navbar = () => {
     return (
-        <div className="flex items-center gap-x-4 p-5 bg-green-500">
-            <div className="hidden lg:flex lg:flex-1 bg-yellow-500">
-                Search
+        <div className="flex items-center gap-x-4 p-5">
+            <div className="hidden lg:flex lg:flex-1">
+                <SearchInput />
+            </div>
+            <div className="block lg:hidden flex-1">
+                <OrganizationSwitcher
+                    hidePersonal
+                    appearance={{
+                        elements: {
+                            rootBox: {
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                width: "100%",
+                                maxWidth: "376px",
+                            },
+                            organizationSwitcherTrigger: {
+                                padding: "6px",
+                                width: "100%",
+                                borderRadius: "8px",
+                                border: "1px solid #E5E7EB",
+                                justifyContent: "space-between",
+                                backgroundColor: "white",
+                            },
+                        },
+                    }}
+                />
             </div>
             <UserButton />
         </div>
---
+++ app/(dashboard)/_components/search-input.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import qs from "query-string";
+import { useRouter } from "next/navigation";
+import { ChangeEvent, useEffect } from "react";
+import { Input } from "@/components/ui/input";
+import { Search } from "lucide-react";
+import { useDebounceValue } from "usehooks-ts";
+
+export const SearchInput = () => {
+    const router = useRouter();
+    const [value, setValue] = useDebounceValue("", 500);
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setValue(e.target.value);
+    };
+
+    useEffect(() => {
+        const url = qs.stringifyUrl(
+            {
+                url: "/",
+                query: {
+                    search: value,
+                },
+            },
+            {
+                skipEmptyString: true,
+                skipNull: true,
+            }
+        );
+
+        router.push(url);
+    }, [value, router]);
+
+    return (
+        <div className="w-full relative">
+            <Search className="absolute top-1/2 left-3 transform -translate-y-1/2 text-muted-foreground h-4 w-4" />
+            <Input
+                className="w-full max-w-[516px] pl-9"
+                placeholder="Search boards"
+                onChange={handleChange}
+            />
+        </div>
+    );
+};
