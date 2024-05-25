e4b31e1d1403f58e26fdf075cc9af66b4bc16b63
Jatin Ranpariya <jatinranpariya1510@gmail.com>
2024-05-25 10:39:55 +0530

    Added Basic Layout of Canvas!

---
+++ app/board/[boardId]/_components/info.tsx
@@ -0,0 +1,9 @@
+const Info = () => {
+    return (
+        <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md">
+            TODO: Information about the board
+        </div>
+    );
+};
+
+export default Info;
---
+++ app/board/[boardId]/_components/participants.tsx
@@ -0,0 +1,9 @@
+const Participants = () => {
+    return (
+        <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md">
+            List of users
+        </div>
+    );
+};
+
+export default Participants;
