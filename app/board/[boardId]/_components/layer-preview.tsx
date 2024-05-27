dd09ec39a732115156de617cc54e27702b9fdc0d
Jatin Ranpariya <jatinranpariya1510@gmail.com>
2024-05-27 15:33:00 +0530

    feat: Add sticky note component

--- app/board/[boardId]/_components/layer-preview.tsx
+++ app/board/[boardId]/_components/layer-preview.tsx
@@ -6,6 +6,7 @@ import { memo } from "react";
 import { Rectangle } from "./rectangle";
 import { Ellipse } from "./ellipse";
 import { Text } from "./text";
+import { Note } from "./note";
 
 interface LayerPreviewProps {
     id: string;
@@ -19,6 +20,15 @@ export const LayerPreview = memo(
         if (!layer) return null;
 
         switch (layer.type) {
+            case LayerType.Note:
+                return (
+                    <Note
+                        id={id}
+                        layer={layer}
+                        onPointerDown={onLayerPointerDown}
+                        selectionColor={selectionColor}
+                    />
+                );
             case LayerType.Text:
                 return (
                     <Text
