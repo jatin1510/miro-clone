4d010e3faaf230ab215b64f2fed5361710dacf07
Jatin Ranpariya <jatinranpariya1510@gmail.com>
2024-05-27 16:36:29 +0530

    chore: Update favorites prop type to string in DashboardPageProps

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -7,7 +7,7 @@ import { BoardList } from "./_components/board-list";
 interface DashboardPageProps {
     searchParams: {
         search?: string;
-        favorites?: boolean;
+        favorites?: string;
     };
 }
 
