b2aa095dc7f20fbf687de5c1cec3a877477524b2
Jatin Ranpariya <jatinranpariya1510@gmail.com>
2024-05-27 16:31:40 +0530

    Refactor: Replaced deprecated toArray with toImmutable

--- app/board/[boardId]/_components/selection-tools.tsx
+++ app/board/[boardId]/_components/selection-tools.tsx
@@ -23,7 +23,7 @@ export const SelectionTools = memo(
             ({ storage }) => {
                 const liveLayerIds = storage.get("layerIds");
                 const indices: number[] = [];
-                const arr = liveLayerIds.toArray();
+                const arr = liveLayerIds.toImmutable();
 
                 for (let i = 0; i < arr.length; i++) {
                     if (selection.includes(arr[i])) {
@@ -45,7 +45,7 @@ export const SelectionTools = memo(
             ({ storage }) => {
                 const liveLayerIds = storage.get("layerIds");
                 const indices: number[] = [];
-                const arr = liveLayerIds.toArray();
+                const arr = liveLayerIds.toImmutable();
 
                 for (let i = 0; i < arr.length; i++) {
                     if (selection.includes(arr[i])) {
