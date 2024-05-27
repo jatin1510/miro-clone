f4e1a36173eda0709ec398b9f4b99a42e8f952b1
Jatin Ranpariya <jatinranpariya1510@gmail.com>
2024-05-27 15:01:47 +0530

    feat: Add selection net to canvas for multi-selection functionality

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -335,6 +335,26 @@ export const Canvas = ({ boardId }: CanvasProps) => {
                     <SelectionBox
                         onResizeHandlePointerDown={onResizeHandlePointerDown}
                     />
+                    {canvasState.mode === CanvasMode.SelectionNet &&
+                        canvasState.current && (
+                            <rect
+                                className="fill-blue-500/5 stroke-blue-500 stroke-1"
+                                x={Math.min(
+                                    canvasState.origin.x,
+                                    canvasState.current.x
+                                )}
+                                y={Math.min(
+                                    canvasState.origin.y,
+                                    canvasState.current.y
+                                )}
+                                width={Math.abs(
+                                    canvasState.origin.x - canvasState.current.x
+                                )}
+                                height={Math.abs(
+                                    canvasState.origin.y - canvasState.current.y
+                                )}
+                            />
+                        )}
                     <CursorsPresence />
                 </g>
             </svg>
