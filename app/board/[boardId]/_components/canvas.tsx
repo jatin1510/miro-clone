1b452a4b2933f2a7442e4b9fb72d50d6b1a219e5
Jatin Ranpariya <jatinranpariya1510@gmail.com>
2024-05-27 14:27:46 +0530

    feat: Add multi-selection functionality to canvas

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -26,6 +26,7 @@ import {
 import { CursorsPresence } from "./cursors-presence";
 import {
     connectionIdToColor,
+    findIntersectingLayersWithRectangle,
     pointerEventToCanvasPoint,
     resizeBounds,
 } from "@/lib/utils";
@@ -35,6 +36,7 @@ import { SelectionBox } from "./selection-box";
 import { SelectionTools } from "./selection-tools";
 
 const MAX_LAYERS = 100;
+const SELECTION_NET_THRESHOLD = 5;
 
 interface CanvasProps {
     boardId: string;
@@ -125,6 +127,32 @@ export const Canvas = ({ boardId }: CanvasProps) => {
         }
     }, []);
 
+    const updateSelectionNet = useMutation(
+        ({ storage, setMyPresence }, current: Point, origin: Point) => {
+            const layers = storage.get("layers").toImmutable();
+            setCanvasState({ mode: CanvasMode.SelectionNet, origin, current });
+
+            const ids = findIntersectingLayersWithRectangle(
+                layerIds,
+                layers,
+                origin,
+                current
+            );
+
+            setMyPresence({ selection: ids });
+        },
+        [layerIds]
+    );
+
+    const startMultiSelection = useCallback((current: Point, origin: Point) => {
+        if (
+            Math.abs(current.x - origin.x) + Math.abs(current.y - origin.y) >
+            SELECTION_NET_THRESHOLD
+        ) {
+            setCanvasState({ mode: CanvasMode.SelectionNet, origin, current });
+        }
+    }, []);
+
     const resizeSelectedLayer = useMutation(
         ({ storage, self }, point: Point) => {
             if (canvasState.mode !== CanvasMode.Resizing) {
@@ -173,7 +201,11 @@ export const Canvas = ({ boardId }: CanvasProps) => {
             e.preventDefault();
             const current = pointerEventToCanvasPoint(e, camera);
 
-            if (canvasState.mode === CanvasMode.Translating) {
+            if (canvasState.mode === CanvasMode.Pressing) {
+                startMultiSelection(current, canvasState.origin);
+            } else if (canvasState.mode === CanvasMode.SelectionNet) {
+                updateSelectionNet(current, canvasState.origin);
+            } else if (canvasState.mode === CanvasMode.Translating) {
                 translateSelectedLayers(current);
             } else if (canvasState.mode === CanvasMode.Resizing) {
                 resizeSelectedLayer(current);
