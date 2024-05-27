33f960cd65af399203deb4b798858a9801bfae89
Jatin Ranpariya <jatinranpariya1510@gmail.com>
2024-05-27 12:33:07 +0530

    Feature: Color change of selected layer

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -32,6 +32,7 @@ import {
 import { LiveObject } from "@liveblocks/client";
 import { LayerPreview } from "./layer-preview";
 import { SelectionBox } from "./selection-box";
+import { SelectionTools } from "./selection-tools";
 
 const MAX_LAYERS = 100;
 
@@ -269,6 +270,10 @@ export const Canvas = ({ boardId }: CanvasProps) => {
                 canRedo={canRedo}
                 undo={history.undo}
                 redo={history.redo}
+            />
+            <SelectionTools
+                camera={camera}
+                setLastUsedColor={setLastUsedColor}
             />
             <svg
                 className="h-[100vh] w-[100vw]"
---
+++ app/board/[boardId]/_components/selection-tools.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { useSelectionBounds } from "@/hooks/use-selection-bounds";
+import { useMutation, useSelf } from "@/liveblocks.config";
+import { Camera, Color } from "@/types/canvas";
+import { memo } from "react";
+import { ColorPicker } from "./color-picker";
+
+interface SelectionToolsProps {
+    camera: Camera;
+    setLastUsedColor: (color: Color) => void;
+}
+
+export const SelectionTools = memo(
+    ({ camera, setLastUsedColor }: SelectionToolsProps) => {
+        const selection = useSelf((me) => me.presence.selection);
+
+        const setFill = useMutation(({ storage }, fill: Color) => {
+            const liveLayers = storage.get("layers");
+            setLastUsedColor(fill);
+            if (!selection) {
+                return;
+            }
+            selection.forEach((id) => liveLayers.get(id)?.set("fill", fill));
+        }, [selection, setLastUsedColor]);
+
+        const selectionBounds = useSelectionBounds();
+
+        if (!selectionBounds) {
+            return null;
+        }
+
+        const x = selectionBounds.width / 2 + selectionBounds.x + camera.x;
+        const y = selectionBounds.y + camera.y;
+
+        return (
+            <div
+                className="absolute p-3 rounded-xl bg-white shadow-sm border flex select-none"
+                style={{
+                    transform: `translate(
+                    calc(${x}px - 50%),
+                    calc(${y - 16}px - 100%)
+                )`,
+                }}
+            >
+                <ColorPicker onChange={setFill} />
+            </div>
+        );
+    }
+);
+
+SelectionTools.displayName = "SelectionTools";
