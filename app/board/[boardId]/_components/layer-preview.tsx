97d7e43c3ee163bd941ecb2700462b39d1fa38ce
Jatin Ranpariya <jatinranpariya1510@gmail.com>
2024-05-27 15:19:47 +0530

    feat: Add Text component for displaying text layers

--- app/board/[boardId]/_components/layer-preview.tsx
+++ app/board/[boardId]/_components/layer-preview.tsx
@@ -5,6 +5,7 @@ import { LayerType } from "@/types/canvas";
 import { memo } from "react";
 import { Rectangle } from "./rectangle";
 import { Ellipse } from "./ellipse";
+import { Text } from "./text";
 
 interface LayerPreviewProps {
     id: string;
@@ -18,6 +19,15 @@ export const LayerPreview = memo(
         if (!layer) return null;
 
         switch (layer.type) {
+            case LayerType.Text:
+                return (
+                    <Text
+                        id={id}
+                        layer={layer}
+                        onPointerDown={onLayerPointerDown}
+                        selectionColor={selectionColor}
+                    />
+                );
             case LayerType.Ellipse:
                 return (
                     <Ellipse
