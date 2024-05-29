e23cfc4008fbab61564d8a973d27103d30b8d29f
Jatin Ranpariya <jatinranpariya1510@gmail.com>
2024-05-29 10:42:06 +0530

    feat: Update app title and document title on BoardIdPage

--- app/board/[boardId]/page.tsx
+++ app/board/[boardId]/page.tsx
@@ -1,11 +1,18 @@
+"use client";
+
 import { Canvas } from "./_components/canvas";
 import { Room } from "@/components/room";
 import { Loading } from "./_components/loading";
+import { useEffect } from "react";
 interface BoardIdPageProps {
     params: { boardId: string };
 }
 
 const BoardIdPage = ({ params }: BoardIdPageProps) => {
+    useEffect(() => {
+        document.title = `Board - Miro Clone`;
+    }, []);
+
     return (
         <Room roomId={params.boardId} fallback={<Loading />}>
             <Canvas boardId={params.boardId} />
