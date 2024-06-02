84335b71a7f64ea55c067e610e6be0e3bf0b2d52
Jatin Ranpariya <jatinranpariya1510@gmail.com>
2024-06-02 10:11:15 +0530

    feat: Select layers only when fully inside the selection rectangle

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,13 @@
-import { Camera, Color, Layer, LayerType, PathLayer, Point, Side, XYWH } from "@/types/canvas";
+import {
+    Camera,
+    Color,
+    Layer,
+    LayerType,
+    PathLayer,
+    Point,
+    Side,
+    XYWH,
+} from "@/types/canvas";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
@@ -60,6 +69,43 @@ export function resizeBounds(bounds: XYWH, corner: Side, point: Point): XYWH {
     return result;
 }
 
+// export function findIntersectingLayersWithRectangle(
+//     layerIds: readonly string[],
+//     layers: ReadonlyMap<string, Layer>,
+//     a: Point,
+//     b: Point
+// ) {
+//     const rect = {
+//         x: Math.min(a.x, b.x),
+//         y: Math.min(a.y, b.y),
+//         width: Math.abs(a.x - b.x),
+//         height: Math.abs(a.y - b.y),
+//     };
+
+//     const ids = [];
+
+//     for (const layerId of layerIds) {
+//         const layer = layers.get(layerId);
+
+//         if (layer == null) {
+//             continue;
+//         }
+
+//         const { x, y, height, width } = layer;
+
+//         if (
+//             rect.x + rect.width > x &&
+//             rect.x < x + width &&
+//             rect.y + rect.height > y &&
+//             rect.y < y + height
+//         ) {
+//             ids.push(layerId);
+//         }
+//     }
+
+//     return ids;
+// }
+
 export function findIntersectingLayersWithRectangle(
     layerIds: readonly string[],
     layers: ReadonlyMap<string, Layer>,
@@ -85,10 +131,10 @@ export function findIntersectingLayersWithRectangle(
         const { x, y, height, width } = layer;
 
         if (
-            rect.x + rect.width > x &&
-            rect.x < x + width &&
-            rect.y + rect.height > y &&
-            rect.y < y + height
+            rect.x <= x &&
+            rect.x + rect.width >= x + width &&
+            rect.y <= y &&
+            rect.y + rect.height >= y + height
         ) {
             ids.push(layerId);
         }
